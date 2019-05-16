f4385359c06853e5034e93093222db0f48107a9a
Administrator <lisicong97@gmail.com>
2019-05-16 20:32:52 +0800

    use neo4j

--- LSTM/test.py
+++ LSTM/test.py
@@ -4,5 +4,11 @@ import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('test.csv', sep=',')
-print(df.head(5))
+
+def a():
+    print(12)
+
+
+if __name__=="__main__":
+    df = pd.read_csv('test.csv', sep=',')
+    print(df.head(5))
---
+++ json2neo4j.py
@@ -0,0 +1,30 @@
+from py2neo import Graph, Node, Relationship
+graph = Graph("http://localhost:7474", username="graph", password='123456')
+with open("baike.json", encoding='utf-8') as f:
+    data = f.read()
+    data = data.replace('\n','')
+    data = eval(data)
+    data = [single['objection'] for single in data]
+'''
+# 提取所有实体 data[i]['key']，用set防止重复
+entity = set()
+for tot in data:
+    for key in tot.keys():
+        if key != 'description':
+            entity.add(tot[key])
+for x in entity:
+    tem = Node(x)
+    graph.create(tem)
+# 部分实体加入description
+
+# 提取所有关系
+
+'''
+for tot in data:
+    obj = Node(tot['obj'])
+    for key in tot.keys():
+        if key != 'obj' and key != 'description':
+            tem = Node(tot[key])
+            r = Relationship(obj, key, tem)
+            s = obj | tem | r
+            graph.create(s)
