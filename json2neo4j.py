e90b3041aaf0dba73d6efb55b373e96f4851222f
Administrator <lisicong97@gmail.com>
2019-05-20 14:46:18 +0800

    use neo4j operation

--- json2neo4j.py
+++ json2neo4j.py
@@ -1,10 +1,13 @@
 from py2neo import Graph, Node, Relationship
+import pandas as pd
+
 graph = Graph("http://localhost:7474", username="graph", password='123456')
 with open("baike.json", encoding='utf-8') as f:
     data = f.read()
     data = data.replace('\n','')
     data = eval(data)
     data = [single['objection'] for single in data]
+
 '''
 # 提取所有实体 data[i]['key']，用set防止重复
 entity = set()
@@ -19,7 +22,7 @@ for x in entity:
 
 # 提取所有关系
 
-'''
+
 for tot in data:
     obj = Node(tot['obj'])
     for key in tot.keys():
@@ -28,3 +31,13 @@ for tot in data:
             r = Relationship(obj, key, tem)
             s = obj | tem | r
             graph.create(s)
+
+'''
+obj = []
+desc = []
+for tot in data:
+    obj.append('\"'+tot['obj']+'\"')
+    desc.append('\"'+tot['description']+'\"')
+
+dataframe = pd.DataFrame({obj: obj, desc: desc})
+dataframe.to_csv("test.csv", index=False, sep=',')
