0a78fbdd152e3c03939b9cd0a55a57555792f1d7
Administrator <lisicong97@gmail.com>
2019-06-25 11:07:35 +0800

    add keras bert

---
+++ bert-master/kerasTest.py
@@ -0,0 +1,107 @@
+import json
+import numpy as np
+import pandas as pd
+from random import choice
+from keras_bert import load_trained_model_from_checkpoint, Tokenizer
+import re, os
+import codecs
+
+
+maxlen = 100
+config_path = './chinese_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = './chinese_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = './chinese_L-12_H-768_A-12/vocab.txt'
+
+
+token_dict = {}
+
+with codecs.open(dict_path, 'r', 'utf8') as reader:
+    for line in reader:
+        token = line.strip()
+        token_dict[token] = len(token_dict)
+
+
+class OurTokenizer(Tokenizer):
+    def _tokenize(self, text):
+        R = []
+        for c in text:
+            if c in self._token_dict:
+                R.append(c)
+            elif self._is_space(c):
+                R.append('[unused1]') # space类用未经训练的[unused1]表示
+            else:
+                R.append('[UNK]') # 剩余的字符是[UNK]
+        return R
+
+tokenizer = OurTokenizer(token_dict)
+
+data = []
+f = open('./data/train.csv','r', encoding='utf-8')
+for line in f.readlines():
+    l = line.split('\t')
+    data.append((l[0].replace(' ',''),l[1][5]))
+train_data = data[:int(len(data)*0.9)]
+valid_data = data[int(len(data)*0.9):]
+
+def seq_padding(X, padding=0):
+    L = [len(x) for x in X]
+    ML = max(L)
+    return np.array([
+        np.concatenate([x, [padding] * (ML - len(x))]) if len(x) < ML else x for x in X
+    ])
+
+
+class data_generator:
+    def __init__(self, data, batch_size=32):
+        self.data = data
+        self.batch_size = batch_size
+        self.steps = len(self.data) // self.batch_size
+        if len(self.data) % self.batch_size != 0:
+            self.steps += 1
+    def __len__(self):
+        return self.steps
+    def __iter__(self):
+        while True:
+            idxs = range(len(self.data))
+            np.random.shuffle(idxs)
+            X1, X2, Y = [], [], []
+            for i in idxs:
+                d = self.data[i]
+                text = d[0][:maxlen]
+                x1, x2 = tokenizer.encode(first=text)
+                y = d[1]
+                X1.append(x1)
+                X2.append(x2)
+                Y.append([y])
+                if len(X1) == self.batch_size or i == idxs[-1]:
+                    X1 = seq_padding(X1)
+                    X2 = seq_padding(X2)
+                    Y = seq_padding(Y)
+                    yield [X1, X2], Y
+                    [X1, X2, Y] = [], [], []
+
+
+from keras.layers import *
+from keras.models import Model
+import keras.backend as K
+from keras.optimizers import Adam
+bert_model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
+
+
+for l in bert_model.layers:
+    l.trainable = True
+
+x1_in = Input(shape=(None,))
+x2_in = Input(shape=(None,))
+
+x = bert_model([x1_in, x2_in])
+x = Lambda(lambda x: x[:, 0])(x)
+p = Dense(1, activation='sigmoid')(x)
+
+model = Model([x1_in, x2_in], p)
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=Adam(1e-5), # 用足够小的学习率
+    metrics=['accuracy']
+)
+model.summary()
